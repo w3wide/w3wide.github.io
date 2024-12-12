470091f5b23fc4aa7e6265b0343177b378865316
Hasrat <42956453+hasrat-ali@users.noreply.github.com>
2024-12-13 01:51:30 +0530

    feat: Use NextUI for base design

---
+++ config/site.ts
@@ -0,0 +1,35 @@
+export const siteConfig = {
+  name: "W3Wide",
+  description: "Make beautiful websites regardless of your design experience.",
+  navItems: [
+    {
+      label: "Home",
+      href: "/",
+    },
+    {
+      label: "Docs",
+      href: "/docs",
+    },
+    {
+      label: "Blog",
+      href: "/blog",
+    },
+    {
+      label: "About",
+      href: "/about",
+    },
+  ],
+  navMenuItems: [
+    {
+      label: "Home",
+      href: "/",
+    },
+  ],
+  links: {
+    github: "https://github.com/nextui-org/nextui",
+    twitter: "https://twitter.com/getnextui",
+    docs: "https://nextui.org",
+    discord: "https://discord.gg/9b6yyZKmH4",
+    sponsor: "https://patreon.com/jrgarciadev",
+  },
+};
---
+++ postcss.config.js
@@ -0,0 +1,9 @@
+/** @type {{plugins: {tailwindcss: {}, autoprefixer: {}}}} */
+const config = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+};
+
+export default config;
